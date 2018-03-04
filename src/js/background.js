e894eb085bb72345e314d25869cc301c33a7d724
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2018-03-05 01:21:57 +0300

    Base front-end is implemented. Extension is now working by opening a new tab and a new HTML file in that tab which shows lineage graphically.

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,4 @@
 chrome.pageAction.onClicked.addListener(function(tab) {
-    console.log("test");
     let action_url = "file:///home/yvz/soyagaci-extension/index.html";
     chrome.tabs.create({url: action_url});
 });
