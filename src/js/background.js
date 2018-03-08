37804c4e4583bf907f37d08346ab48d1763472d4
Oğuzcan Yavuz <oguzcanyavuz@hotmail.com>
2018-03-08 14:43:55 +0300

    Getting the source page functions added but there is a bug at using chrome.tabs. It returns undefined.

--- src/js/background.js
+++ src/js/background.js
@@ -1,16 +1,35 @@
-import { HTMLParser } from '@soyagaci/parser/format/html';
-
-chrome.pageAction.onClicked.addListener(function(tab) {
-    let action_url = "file:///home/yvz/soyagaci-extension/dist/index.html";
-    chrome.tabs.create({url: action_url}).then(function(tab) {
-        HTMLParser("<h1>test</h1>").then(console.log);
-    })
-});
+import {HTMLParser} from '@soyagaci/parser/format/html';
 
 function checkForValidUrl(tabId, changeInfo, tab) {
-    if(tab.url.indexOf("https://www.turkiye.gov.tr/nvi-alt-ust-soy-bilgisi-sorgulama") === 0) {
+    let eDevlet = "https://www.turkiye.gov.tr/nvi-alt-ust-soy-bilgisi-sorgulama";
+    let test = "";
+    if (tab.url.indexOf(eDevlet === 0)) {
         chrome.pageAction.show(tabId);
     }
 }
 
+function onWindowLoad() {
+    chrome.tabs.executeScript(null, {
+        file: "getPagesSource.js"
+    }, function () {
+        // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+        if (chrome.runtime.lastError)
+            console.log(chrome.runtime.lastError.message);
+    });
+}
+
 chrome.tabs.onUpdated.addListener(checkForValidUrl);
+
+chrome.pageAction.onClicked.addListener(function (tab) {
+    let actionUrl = "file:///home/yvz/soyagaci-extension/dist/index.html";
+    let htmlResult = "";
+    chrome.runtime.onMessage.addListener(function (request, sender) {
+        if (request.action == "getSource") {
+            htmlResult = request.source;
+        }
+    });
+    window.onload = onWindowLoad;
+    chrome.tabs.create({url: actionUrl}).then(function (tab) {
+        HTMLParser(htmlResult).then(console.log);
+    })
+});
